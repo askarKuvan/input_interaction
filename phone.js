b7e75cee9f0bb51a0cd9c490c98df584e63a6386
Askar K <askar_kuvanychbekov1@baylor.edu>
2019-07-16 00:51:51 -0500

    Assignment1

--- phone.js
+++ phone.js
@@ -1,42 +1,55 @@
-$(document).ready(function() 
+var btnContainer = document.getElementById("tabs_div");
+var btns = btnContainer.getElementsByClassName("tabButtons");
+for (var i = 0; i < btns.length; i++)
+{
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
+$(document).ready(function()
 { // do this when the document is loaded
     $("#content_dialer").show(); // show the element with ID "element"
     $("#content_contactList").hide(); // hide the element with ID "otherElement"
     $("#content_addContact").hide(); // hide the element with ID "otherElement"
 
-
-    var contacts = ["Robert Drake","Emma Frost", "Doreen Green", "Jean Grey",
-    "James Howlett","Ororo Munroe", "Katherine Pryde","Piotr Rasputin",
-    "Scott Summers","Kurt Wagner","Warren Washington","Charles Xavier"];
-    var listOfContacts = document.getElementById("listOfContacts");
-    var listItems = listOfContacts.map( (element) => {
-    `<li id='${element} 'class='listItem'>${element}</li>` });
-    listOfContacts.innerHTML = listItems.join('');	
-
-    for( var i=0;i<contacts.length; i++)
-    {
-      var name = contacts[i];
-      var contact_name = document.createElement("li");
-      listItem.textContent = name;
-      ul.appendChild(contact_name);
-    }
+    // var contacts = ["Robert Drake","Emma Frost", "Doreen Green", "Jean Grey",
+    // "James Howlett","Ororo Munroe", "Katherine Pryde","Piotr Rasputin",
+    // "Scott Summers","Kurt Wagner","Warren Washington","Charles Xavier"];
+    // var listOfContacts = document.getElementById("listOfContacts");
+    // var listItems = listOfContacts.map( (element) => {
+    // `<li id='${element} 'class='listItem'>${element}</li>` });
+    // listOfContacts.innerHTML = listItems.join('');
+    //
+    // for( var i=0;i<contacts.length; i++)
+    // {
+    //   var name = contacts[i];
+    //   var contact_name = document.createElement("li");
+    //   listItem.textContent = name;
+    //   ul.appendChild(contact_name);
+    // }
 });
 
-$("#dialer").click(function() 
+$("#dialer").click(function()
 { // when "button_id" is clicked
     $("#content_dialer").show(); // show the element with ID "element"
     $("#content_contactList").hide(); // hide the element with ID "otherElement"
     $("#content_addContact").hide(); // hide the element with ID "otherElement"
+
+
 });
 
-$("#contactList").click(function() 
+$("#contactList").click(function()
 { // when "button_id" is clicked
     $("#content_contactList").show(); // show the element with ID "element"
     $("#content_dialer").hide(); // hide the element with ID "otherElement"
     $("#content_addContact").hide(); // hide the element with ID "otherElement"
+
 });
 
-$("#addContact").click(function() 
+$("#addContact").click(function()
 { // when "button_id" is clicked
     $("#content_addContact").show(); // show the element with ID "element"
     $("#content_contactList").hide(); // hide the element with ID "otherElement"
